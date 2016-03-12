2b5bee0f7d6c878e8377b301586da9ca1ac67bd1
richardyam1 <richardyam1@yahoo.com>
2016-03-12 16:07:52 -0500

    Open file and put down psudocode.

--- pset4/jpg/recover.c
+++ pset4/jpg/recover.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * recover.c
  *
@@ -7,7 +9,28 @@
  * Recovers JPEGs from a forensic image.
  */
 
+/*
+psudocode:
+open card file
+repeat until end of card
+    read 512 bytes into a buffer
+    if start of new jpg
+        .....
+    else if already found jpg    
+        .....
+close file
+*/
+
 int main(int argc, char* argv[])
 {
     // TODO
+    //Opens the file
+    FILE* file = fopen("card.raw", "r");
+    
+    //Ends the program if NULL
+    if (file == NULL)
+    {
+        printf("Could not open file \n");
+        return 1;
+    }
 }
